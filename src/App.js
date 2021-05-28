0f3538911eef37abc031235455236adc25296780
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-05-29 02:13:45 +0530

    feat: rows

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>netflix-clone</h1>
+      <Row
+        title='NETFLIX ORIGINALS'
+        fetchURL={requests.fetchNetflixOriginals} 
+        isLargeRow
+      />
+      <Row title='Trending Now' fetchURL={requests.fetchTrending} />
+      <Row title='Top Rated' fetchURL={requests.fetchTopRated} />
+      <Row title='Action Movies' fetchURL={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchURL={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchURL={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchURL={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
