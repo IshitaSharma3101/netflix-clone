b81892e5c43cb501e31dfadcb5aa8f3f69eca3c8
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-05-30 03:35:32 +0530

    style: banner contents

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -9,8 +10,10 @@ function Banner() {
     async function fetchData() {
       const request = await axios.get(requests.fetchNetflixOriginals);
       setMovie(
-          request.data.results[
-              Math.floor(Math.random() * request.data.results.length)]);
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ],
+      );
       return request;
     }
     fetchData();
@@ -18,29 +21,28 @@ function Banner() {
 
   console.log(movie);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n-1) + "..." : str;
+  }
 
-  return <header className="banner"
-            style = {{
-                backgroundSize: "cover",
-                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-                backgroundPosition: "center center",
-            }}
-        >
-      <div className="banner__contents">
-            <h1>
-                {movie?.title || movie?.name || movie?.original_name}
-            </h1>
-            <div className="banner__buttons">
-                <button className="banner__button">
-                    Play
-                </button>
-                <button className="banner__button">
-                    My List
-                </button>
-            </div>
-            <h1 className="banner__description">{movie?.overview}</h1>
+  return (
+    <header
+      className='banner'
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}>
+      <div className='banner__contents'>
+        <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+        <div className='banner__buttons'>
+          <button className='banner__button'>Play</button>
+          <button className='banner__button'>My List</button>
+        </div>
+        <h1 className='banner__description'>{truncate(movie?.overview, 150)}</h1>
       </div>
-  </header>;
+    </header>
+  );
 }
 
 export default Banner;
