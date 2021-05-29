d516f4727394834edad02e37db173549292cd549
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-05-30 03:54:10 +0530

    style: banner done

--- src/Banner.js
+++ src/Banner.js
@@ -41,6 +41,7 @@ function Banner() {
         </div>
         <h1 className='banner__description'>{truncate(movie?.overview, 150)}</h1>
       </div>
+      <div className="banner--fadeBottom"></div>
     </header>
   );
 }
