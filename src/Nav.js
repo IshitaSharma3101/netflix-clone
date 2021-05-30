6a46330941f84af0598c20a05fdcf49aafc68c26
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-05-30 21:52:53 +0530

    style: navbar done

--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,33 @@
-import React from 'react'
-import "./Nav.css"
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
 
 function Nav() {
-    return (
-      <div className='nav'>
-        <img
-          className='nav__logo'
-          src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
-          alt='Netflix Logo'
-        />
-        <img
-          className='nav__avatar'
-          src='https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png'
-          alt='User Image'
-        />
-      </div>
-    );
+    const [show, setShow] = useState(false)
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else setShow(false);
+    });
+    return()=>{
+        window.removeEventListener("scroll");
+    }
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className='nav__logo'
+        src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
+        alt='Netflix Logo'
+      />
+      <img
+        className='nav__avatar'
+        src='https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png'
+        alt='User Image'
+      />
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
